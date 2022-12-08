d8d05ad15fec85588da5cf4f3e68492fdc63c59e
Gincoolwant <soulbox790326@gmail.com>
2022-12-08 20:30:43 +0800

    feat add amdin api

--- controllers/pages/admin-controller.js
+++ controllers/pages/admin-controller.js
@@ -1,5 +1,3 @@
-const { Restaurant, User, Category } = require('../../models')
-const { imgurFileHandler } = require('../../helpers/file-helpers')
 const adminServices = require('../../services/admin-services')
 
 const adminController = {
@@ -7,66 +5,29 @@ const adminController = {
     adminServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('admin/restaurants', data))
   },
   createRestaurant: (req, res, next) => {
-    return Category.findAll({
-      raw: true
-    })
-      .then(categories => res.render('admin/create-restaurant', { categories }))
-      .catch(err => next(err))
+    adminServices.createRestaurant(req, (err, data) => err ? next(err) : res.render('admin/create-restaurant', data))
   },
   postRestaurant: (req, res, next) => {
-    adminServices.deleteRestaurant(req, (err, data) => {
+    adminServices.postRestaurant(req, (err, data) => {
       if (err) return next(err)
       req.flash('success_messages', 'restaurant was successfully created')
-      req.session.createdData = data
+      req.session.createdRestaurant = data
       res.redirect('/admin/restaurants')
     })
   },
   getRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
-      raw: true,
-      nest: true,
-      include: [Category]
-    })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        return res.render('admin/restaurant', { restaurant })
-      })
-      .catch(err => next(err))
+    adminServices.getRestaurant(req, (err, data) => err ? next(err) : res.render('admin/restaurant', data))
   },
   editRestaurant: (req, res, next) => {
-    return Promise.all([
-      Restaurant.findByPk(req.params.id, { raw: true }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurant, categories]) => {
-        if (!restaurant) throw new Error("Restaurant doesn't exist!")
-        res.render('admin/edit-restaurant', { restaurant, categories })
-      })
-      .catch(err => next(err))
+    adminServices.editRestaurant(req, (err, data) => err ? next(err) : res.render('admin/edit-restaurant', data))
   },
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description, categoryId } = req.body
-    if (!name) throw new Error('Restaurant name is required!')
-    const { file } = req
-    Promise.all([imgurFileHandler(file), Restaurant.findByPk(req.params.id)])
-      .then(([filePath, restaurant]) => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-
-        return restaurant.update({
-          name,
-          tel,
-          address,
-          openingHours,
-          description,
-          image: filePath || restaurant.image,
-          categoryId
-        })
-      })
-      .then(() => {
-        req.flash('success_messages', 'restaurant was successfully updated')
-        res.redirect('/admin/restaurants')
-      })
-      .catch(err => next(err))
+    adminServices.putRestaurant(req, (err, data) => {
+      if (err) return next(err)
+      req.session.updatedRestaurant = data
+      req.flash('success_messages', 'restaurant was successfully updated')
+      res.redirect('/admin/restaurants')
+    })
   },
   deleteRestaurant: (req, res, next) => {
     adminServices.deleteRestaurant(req, (err, data) => {
@@ -77,32 +38,15 @@ const adminController = {
     })
   },
   getUsers: (req, res, next) => {
-    return User.findAll({
-      raw: true
-    })
-      .then(users => {
-        return res.render('admin/users', { users })
-      })
-      .catch(err => next(err))
+    adminServices.getUsers(req, (err, data) => err ? next(err) : res.render('admin/users', data))
   },
   patchUser: (req, res, next) => {
-    return User.findByPk(req.params.id)
-      .then(user => {
-        if (!user) throw new Error("user didn't exist!")
-        if (user.email === 'root@example.com') {
-          req.flash('error_messages', '禁止變更 root 權限')
-          return res.redirect('back')
-        } else {
-          return user.update({
-            isAdmin: !(user.isAdmin)
-          })
-            .then(() => {
-              req.flash('success_messages', '使用者權限變更成功')
-              res.redirect('/admin/users')
-            })
-        }
-      })
-      .catch(err => next(err))
+    adminServices.patchUser(req, (err, data) => {
+      if (err) return next(err)
+      req.session.patchUser = data
+      req.flash('success_messages', '使用者權限變更成功')
+      res.redirect('/admin/users')
+    })
   }
 }
 
--- services/admin-services.js
+++ services/admin-services.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminServices = {
@@ -11,6 +11,45 @@ const adminServices = {
       .then(restaurants => {
         return cb(null, { restaurants })
       })
+      .catch(err => cb(err))
+  },
+  createRestaurant: (req, cb) => {
+    return Category.findAll({
+      raw: true
+    })
+      .then(categories => cb(null, { categories }))
+      .catch(err => cb(err))
+  },
+  editRestaurant: (req, cb) => {
+    return Promise.all([
+      Restaurant.findByPk(req.params.id, { raw: true }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurant, categories]) => {
+        if (!restaurant) throw new Error("Restaurant doesn't exist!")
+        cb(null, { restaurant, categories })
+      })
+      .catch(err => cb(err))
+  },
+  putRestaurant: (req, cb) => {
+    const { name, tel, address, openingHours, description, categoryId } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    const { file } = req
+    Promise.all([imgurFileHandler(file), Restaurant.findByPk(req.params.id)])
+      .then(([filePath, restaurant]) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        return restaurant.update({
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+          image: filePath || restaurant.image,
+          categoryId
+        })
+      })
+      .then(updatedRestaurant => cb(null, { restaurant: updatedRestaurant }))
       .catch(err => cb(err))
   },
   postRestaurant: (req, cb) => {
@@ -32,6 +71,18 @@ const adminServices = {
       .then(newRestaurant => cb(null, { restaurant: newRestaurant }))
       .catch(err => cb(err))
   },
+  getRestaurant: (req, cb) => {
+    Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return cb(null, { restaurant })
+      })
+      .catch(err => cb(err))
+  },
   deleteRestaurant: (req, cb) => {
     Restaurant.findByPk(req.params.id)
       .then(restaurant => {
@@ -41,6 +92,30 @@ const adminServices = {
       })
       .then(deleteRestaurant => {
         return cb(null, { restaurant: deleteRestaurant })
+      })
+      .catch(err => cb(err))
+  },
+  getUsers: (req, cb) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => {
+        cb(null, { users })
+      })
+      .catch(err => cb(err))
+  },
+  patchUser: (req, cb) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("user didn't exist!")
+        if (user.email === 'root@example.com') {
+          throw new Error('禁止變更 root 權限')
+        } else {
+          return user.update({
+            isAdmin: !(user.isAdmin)
+          })
+            .then(updatedUser => cb(null, { user: updatedUser }))
+        }
       })
       .catch(err => cb(err))
   }
