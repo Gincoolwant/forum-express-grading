126e469797e21e4f341d969b3198fe3c3cb8fd50
Gincoolwant <soulbox790326@gmail.com>
2022-12-09 10:33:04 +0800

    feat add admin category api

--- routes/apis/modules/admin.js
+++ routes/apis/modules/admin.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const upload = require('../../../middleware/multer')
 const adminController = require('../../../controllers/apis/admin-controller')
+const categoryController = require('../../../controllers/apis/category-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
@@ -14,5 +15,11 @@ router.delete('/restaurants/:id', adminController.deleteRestaurant)
 
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
+
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
+router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
 
 module.exports = router
